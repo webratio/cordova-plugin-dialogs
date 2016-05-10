47f41b287fb5c3e793749db4fed960404dd81566
Lorenzo Dalla Vecchia <lorenzo.dallavecchia@webratio.com>
2016-05-10 14:57:59 +0200

    Removed useless override of JS methods in WP8 notification.js

--- www/wp8/notification.js
+++ www/wp8/notification.js
@@ -17,22 +17,19 @@
  * specific language governing permissions and limitations
  * under the License.
  *
- */
+*/
 
 var exec = require('cordova/exec');
-var platform = require('cordova/platform');
 
 /**
- * Provides access to notifications on the device.
+ * Provides WP8 enhanced notification API.
  */
-
 module.exports = {
-    
-    
-    /**
+	
+	/**
      * Show an activity dialog
      */
-    activityStart: function (title, message, cancelable) {
+    activityStart : function(title, message, cancelable) {
         // If title and message not specified then mimic Android behavior of
         // using default strings.
         if (typeof title === "undefined" && typeof message == "undefined") {
@@ -40,87 +37,16 @@ module.exports = {
             message = 'Please wait...';
         }
         cancelable = cancelable || false;
-        exec(null, null, 'Notification', 'activityStart', [title, message, cancelable]);
+        exec(null, null, 'Notification', 'activityStart', [ title, message, cancelable ]);
     },
-    
+
     /**
      * Close an activity dialog
      */
-    activityStop: function () {
+    activityStop : function() {
         exec(null, null, 'Notification', 'activityStop', []);
     },
-    
-    /**
-     * Open a native alert dialog, with a customizable title and button text.
-     *
-     * @param {String} message              Message to print in the body of the alert
-     * @param {Function} completeCallback   The callback that is called when user clicks on a button.
-     * @param {String} title                Title of the alert dialog (default: Alert)
-     * @param {String} buttonLabel          Label of the close button (default: OK)
-     */
-    alert: function (message, completeCallback, title, buttonLabel) {
-        var _title = (title || "Alert");
-        var _buttonLabel = (buttonLabel || "OK");
-        exec(completeCallback, null, "Notification", "alert", [message, _title, _buttonLabel]);
-    },
-    
-    /**
-     * Open a native confirm dialog, with a customizable title and button text.
-     * The result that the user selects is returned to the result callback.
-     *
-     * @param {String} message              Message to print in the body of the alert
-     * @param {Function} resultCallback     The callback that is called when user clicks on a button.
-     * @param {String} title                Title of the alert dialog (default: Confirm)
-     * @param {Array} buttonLabels          Array of the labels of the buttons (default: ['OK', 'Cancel'])
-     */
-    confirm: function (message, resultCallback, title, buttonLabels) {
-        var _title = (title || "Confirm");
-        var _buttonLabels = (buttonLabels || ["OK", "Cancel"]);
-        
-        // Strings are deprecated!
-        if (typeof _buttonLabels === 'string') {
-            console.log("Notification.confirm(string, function, string, string) is deprecated.  Use Notification.confirm(string, function, string, array).");
-        }
-        
-        // Some platforms take an array of button label names.
-        // Other platforms take a comma separated list.
-        // For compatibility, we convert to the desired type based on the platform.
-        if (platform.id == "amazon-fireos" || platform.id == "android" || platform.id == "ios" ||
-                platform.id == "windowsphone" || platform.id == "firefoxos" || platform.id == "ubuntu" ||
-                platform.id == "windows8" || platform.id == "windows") {
-            
-            if (typeof _buttonLabels === 'string') {
-                _buttonLabels = _buttonLabels.split(","); // not crazy about changing the var type here
-            }
-        } else {
-            if (Array.isArray(_buttonLabels)) {
-                var buttonLabelArray = _buttonLabels;
-                _buttonLabels = buttonLabelArray.toString();
-            }
-        }
-        exec(resultCallback, null, "Notification", "confirm", [message, _title, _buttonLabels]);
-    },
-    
-    /**
-     * Open a native prompt dialog, with a customizable title and button text.
-     * The following results are returned to the result callback:
-     *  buttonIndex     Index number of the button selected.
-     *  input1          The text entered in the prompt dialog box.
-     *
-     * @param {String} message              Dialog message to display (default: "Prompt message")
-     * @param {Function} resultCallback     The callback that is called when user clicks on a button.
-     * @param {String} title                Title of the dialog (default: "Prompt")
-     * @param {Array} buttonLabels          Array of strings for the button labels (default: ["OK","Cancel"])
-     * @param {String} defaultText          Textbox input value (default: empty string)
-     */
-    prompt: function (message, resultCallback, title, buttonLabels, defaultText) {
-        var _message = (message || "Prompt message");
-        var _title = (title || "Prompt");
-        var _buttonLabels = (buttonLabels || ["OK", "Cancel"]);
-        var _defaultText = (defaultText || "");
-        exec(resultCallback, null, "Notification", "prompt", [_message, _title, _buttonLabels, _defaultText]);
-    },
-    
+
     /**
      * Display a progress dialog with progress bar that goes from 0 to 100.
      *
@@ -129,37 +55,24 @@ module.exports = {
      * @param {String}
      *            message Message to display in the dialog.
      */
-    progressStart: function (title, message) {
-        exec(null, null, 'Notification', 'progressStart', [title, message]);
+    progressStart : function(title, message) {
+        exec(null, null, 'Notification', 'progressStart', [ title, message ]);
     },
-    
+
     /**
      * Close the progress dialog.
      */
-    progressStop: function () {
+    progressStop : function() {
         exec(null, null, 'Notification', 'progressStop', []);
     },
-    
+
     /**
      * Set the progress dialog value.
      *
      * @param {Number}
      *            value 0-100
      */
-    progressValue: function (value) {
-        exec(null, null, 'Notification', 'progressValue', [value]);
-    },
-    
-    /**
-     * Causes the device to beep.
-     * On Android, the default notification ringtone is played "count" times.
-     *
-     * @param {Integer} count       The number of beeps.
-     */
-    beep: function (count) {
-        var defaultedCount = count || 1;
-        exec(null, null, "Notification", "beep", [defaultedCount]);
+    progressValue : function(value) {
+        exec(null, null, 'Notification', 'progressValue', [ value ]);
     }
-    
-    
 };
